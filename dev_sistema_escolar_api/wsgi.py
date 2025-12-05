1723a8690fea0781cded7d7273c5bb7bf36a39db
yaelalfonso31-lang <yael.alfonso31@gmail.com>
2025-12-05 09:58:46 -0600

    commit

--- dev_sistema_escolar_api/wsgi.py
+++ dev_sistema_escolar_api/wsgi.py
@@ -1,21 +1,14 @@
 """
 WSGI config for dev_sistema_escolar_api project.
 """
+"""
+WSGI config for dev_sistema_escolar_api project.
+"""
 
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
 
-# ✅ Agregar el path de tu proyecto
-path = '/home/Alfonso3131/backend_dev_sistema_escolar_api/'  # Cambia esto a tu ruta real
-if path not in sys.path:
-    sys.path.append(path)
-
-# ✅ Cargar variables de entorno (si usas python-dotenv)
-from dotenv import load_dotenv
-project_folder = os.path.expanduser(path)
-load_dotenv(os.path.join(project_folder, '.env'))
-
+# Configura el módulo de settings por defecto
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dev_sistema_escolar_api.settings')
 
 application = get_wsgi_application()
